074d8105d776c60144e69612ea5ee6a9b2a2f63b
davisc0801 <davisc1014@gmail.com>
2019-04-28 14:46:31 -0600

    Add home page route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #two arguments, first is the route taken, the second is the controller # method
+  get '/', to: 'welcome#index'
+
 end
